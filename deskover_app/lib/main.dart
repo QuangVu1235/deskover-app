e7e8335f9c9ac41bcc7e0850aa5953dcd9fc920e
Pham Quang Vu <87456448+QuangVu1235@users.noreply.github.com>
2022-06-29 13:42:20 +0700

    ádá

--- deskover_app/lib/main.dart
+++ deskover_app/lib/main.dart
@@ -3,7 +3,7 @@ import 'package:deskover_app/client/dio_client.dart';
 import 'package:deskover_app/api/user.dart';
 import 'package:deskover_app/api/widget/create.dart';
 import 'package:deskover_app/modules/sign/login.dart';
-import 'package:deskover_app/modules/sign/login_screen.dart';
+import 'package:deskover_app/modules/splashcreen/splashscreen.dart';
 import 'package:deskover_app/themes/themes.dart';
 import 'package:deskover_app/themes/ui_colors.dart';
 import 'package:flutter/material.dart';
@@ -16,12 +16,20 @@ void main()  {
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
 
+  Future<void> loading() async {
+    await Future.delayed(const Duration(seconds: 3));
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return GetMaterialApp(
+      useInheritedMediaQuery: true,
+      // locale: DevicePreview.locale(context),
+      // builder: DevicePreview.appBuilder,
+      debugShowCheckedModeBanner: false,
       title: 'Mutosi App',
       theme: Themes.mainTheme,
-      home:  Login(),
+      home: SplashScreen(action: loading, nextScreen: const Login()),
     );
   }
 }
--- deskover_app/lib/modules/sign/login.dart
+++ deskover_app/lib/modules/sign/login.dart
@@ -9,6 +9,8 @@ import 'package:deskover_app/themes/ui_colors.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
+import '../../themes/space_values.dart';
+
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
 
@@ -21,63 +23,117 @@ class _Login extends State<Login> {
   late final TextEditingController _password;
 
   final DioClient _dioClient = DioClient();
-
   bool isCreating = false;
 
   @override
   void initState() {
     _username = TextEditingController();
     _password = TextEditingController();
-
     super.initState();
   }
 
   @override
   Widget build(BuildContext context) {
+    var heightOfScreen = MediaQuery.of(context).size.height;
+    var widthOfScreen = MediaQuery.of(context).size.width;
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: UIColors.white,
-        title: const Text('Mutosi IOT'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(20),
-        child: Form(
-          child: Column(
-            children: [
-              GlobalInputFormWidget(
-                controller: _username,
-                validator: Validator.phone,
-                textInputType: TextInputType.phone,
-                title: 'Vui lòng nhập tên đăng nhập',
-                hint: 'Vui lòng nhập số điện thoại',
-              ),
-              const SizedBox(
-                height: 20,
-              ),
-              GlobalInputFormWidget(
-                controller: _password,
-                validator: Validator.password,
-                textInputType: TextInputType.visiblePassword,
-                title: 'Mật khẩu',
-                hint: 'Vui lòng nhập mật khẩu',
-                security: true,
-              ),
-              const SizedBox(height: 16.0),
-              isCreating
-                  ? const CircularProgressIndicator()
-                  : ElevatedButton(
-                      onPressed: _onLogin,
-                      child: const Text(
-                        'Login',
-                        style: TextStyle(fontSize: 20.0),
+      body: GestureDetector(
+        child: Container(
+          color: UIColors.black10 ,
+          child: Stack(
+            children:  <Widget>[
+              Container(
+                height: heightOfScreen * 0.45,
+                width: widthOfScreen,
+                decoration: const BoxDecoration(
+                    color: Colors.white,// Set border width
+                    borderRadius: BorderRadius.only( bottomLeft: Radius.circular(40),bottomRight: Radius.circular(40)
+                        ), // Set rounded corner radius
+                    boxShadow: [BoxShadow(blurRadius: 10,color: UIColors.black10,offset: Offset(1,2))] // Make rounded corner of border
+                ),
+                child: Container(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: <Widget>[
+                      SizedBox(
                       ),
-                    ),
+                      Image.asset('resources/images/onlinedelivery_20210504191855.jpg')
+
+                    ],
+                  ),
+                ),
+              ),
+              ListView(
+                padding: EdgeInsets.all(0),
+                children: <Widget>[
+                  SizedBox(
+                    height: heightOfScreen * 0.45,
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.all(42),
+                    child: _form(),
+                  ),
+
+                ],
+              ),
+
             ],
           ),
-        ),
+        )
       ),
     );
-
+  }
+  Widget _form(){
+    return Form(
+      child: Column(
+        children: [
+          GlobalInputFormWidget(
+            controller: _username,
+            validator: Validator.phone,
+            textInputType: TextInputType.phone,
+            title: 'Vui lòng nhập tên đăng nhập',
+            hint: 'Vui lòng nhập số điện thoại',
+          ),
+          const SizedBox(
+            height: 20,
+          ),
+          GlobalInputFormWidget(
+            controller: _password,
+            validator: Validator.password,
+            textInputType: TextInputType.visiblePassword,
+            title: 'Mật khẩu',
+            hint: 'Vui lòng nhập mật khẩu',
+            security: true,
+          ),
+          const SizedBox(height: 16.0),
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 87),
+            child: SizedBox(
+              width: double.infinity,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(54),
+                  ),
+                  primary: UIColors.black70, // background
+                  onPrimary: Colors.white,
+                  // foreground
+                ),
+                onPressed: _onLogin,
+                child: const Padding(
+                  padding: EdgeInsets.all(5),
+                  child: Text('Đăng nhập',style: TextStyle(
+                      color: UIColors.white,
+                      fontSize: 16,
+                      fontWeight: FontWeight.w600
+                  ),),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
   }
   void _onLogin() async {
     setState(() {
